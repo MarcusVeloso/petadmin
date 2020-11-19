705196bcdd5a1320be1d511742a43afbbc947201
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-11-19 20:42:23 -0300

    add feature sell

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
 
   namespace :admin do
+    resources :sells
+    resources :suppliers
+    resources :campains
+    resources :discounts
+    resources :services
+    resources :products
+    resources :clients
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
