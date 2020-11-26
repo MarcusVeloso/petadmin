9efab3654faa4f3635d86888dc9e4b710f42c468
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-11-26 00:18:37 -0300

    add validation sell

--- app/controllers/admin/sells_controller.rb
+++ app/controllers/admin/sells_controller.rb
@@ -2,6 +2,8 @@ module Admin
   class SellsController &lt; Fae::BaseController
     before_action :adjust_enum, only: [:create, :update]
  
+    validates :client, presence: true
+
     def adjust_enum
       params[:sell][:status] = params[:sell][:status].to_i
     end
