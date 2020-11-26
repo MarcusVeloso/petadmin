f72e6e55914d45f2ffe08819d2488aff1cb4522c
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-11-26 00:19:26 -0300

    add validation campaign

--- app/models/campain.rb
+++ app/models/campain.rb
@@ -1,5 +1,9 @@
 class Campain < ApplicationRecord
   include Fae::BaseModelConcern
+
+  has_many :campaign_client
+  has_many :clients, through: :campaign_client
+
   def fae_display_field
     title
   end
