d81240c64469d69a66eef204dd98cd9acdbcdfc9
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-11-26 00:21:14 -0300

    add structure navigations

--- app/models/concerns/fae/navigation_concern.rb
+++ app/models/concerns/fae/navigation_concern.rb
@@ -24,16 +24,20 @@ module Fae
     # end
 
     def structure
-      [
+    [
+      item('Manage', subitems: [
         item('Clients', path: admin_clients_path),
-        item('Products', path: admin_products_path),
-        item('Services', path: admin_services_path),
         item('Discounts', path: admin_discounts_path),
-        item('Campains', path: admin_campains_path),
         item('Suppliers', path: admin_suppliers_path),
+        item('Services', path: admin_services_path),
+        item('Products', path: admin_products_path),
         item('Sells', path: admin_sells_path),
-        # scaffold inject marker
-      ]
+      ]),
+      item('Marketing', subitems: [
+        item('Campaigns', path: admin_campaigns_path)
+      ]),
+      item('+ Sell', path: '/admin/sells/new')
+    ]
     end
 
   end
