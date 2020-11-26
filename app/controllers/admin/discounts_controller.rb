154a584c920c6be14a484de161b2a0eeddf48b4c
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-11-26 00:18:16 -0300

    add validation discount

--- app/controllers/admin/discounts_controller.rb
+++ app/controllers/admin/discounts_controller.rb
@@ -1,5 +1,8 @@
 module Admin
   class DiscountsController < Fae::BaseController
+    
+    validates :title, uniqueness: true, presence: true
+    validates :value, uniqueness: true, presence: true
 
   end
 end
